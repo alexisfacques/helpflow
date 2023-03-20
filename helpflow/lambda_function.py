1f25f46603f1c46d8671c9fb6ca6ffa3c96e0b22
Alexis Facques <alexis.facques@om.fr>
2023-03-20 20:09:42 +0100

    All

--- helpflow/lambda_function.py
+++ helpflow/lambda_function.py
@@ -1,5 +1,7 @@
 """TODO:"""
+from importlib import import_module
 import logging
+import os
 
 # From local modules:
 from custom_connector_sdk.lambda_handler.lambda_handler import BaseLambdaConnectorHandler
@@ -7,15 +9,14 @@ from custom_connector_sdk.lambda_handler.lambda_handler import BaseLambdaConnect
 from .configuration_handler import ConfigurationHandler
 from .helpflow_resolver import HelpFlowResolver
 
+
 LOGGER = logging.getLogger()
 
 
 def lambda_handler(event, context):
-    """
-    :param event:
-    :param context:
-    """
-    LOGGER.info(event)
-    helpflow_resolver: HelpFlowResolver = HelpFlowResolver()
-    configuration_handler: ConfigurationHandler = ConfigurationHandler()
-    return BaseLambdaConnectorHandler(helpflow_resolver, helpflow_resolver, configuration_handler).lambda_handler(event, context)
+    EXTENSIONS_DIR: str = os.environ.get('EXTENSIONS_DIR', 'extensions')
+    import_module(f'{EXTENSIONS_DIR}', __name__)
+
+    helpflow: HelpFlowResolver = HelpFlowResolver()
+    configuration: ConfigurationHandler = ConfigurationHandler()
+    return BaseLambdaConnectorHandler(helpflow, helpflow, configuration).lambda_handler(event, context)
